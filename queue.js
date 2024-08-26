7ee4a0586fe9522c18cc442048a861f97705c567
shalini <shalini.m@ge.com>
2024-08-26 20:02:16 +0530

    Shalini--Stack and queue implimentation

---
+++ queue.js
@@ -0,0 +1,37 @@
+class Queue {
+  constructor() {
+    this.item = {}
+    this.front = 0
+    this.rear = 0
+  }
+  enqueue(element) {
+    this.item[this.rear] = element;
+    this.rear++
+    return element;
+  }
+  peek(){
+   return this.item[this.front]
+  }
+  size(){
+    return this.rear- this.front
+  }
+  isEmpty(){
+    return this.rear- this.front===0
+  }
+  dequeue(){
+    const ele = this.item[this.front]
+    delete this.item[this.front]
+    this.front++
+    return ele
+  }
+}
+
+
+const que = new Queue()
+console.log('que.enqueue(5): ', que.enqueue(5));
+console.log('que.enqueue(5): ', que.enqueue(7));
+console.log('que.enqueue(5): ', que.enqueue(43));
+console.log('que.enqueue(5): ', que.size());
+console.log('que.enqueue(5): ', que.peek());
+console.log('que.enqueue(5): ', que.isEmpty());
+console.log('que.enqueue(5): ', que.dequeue());
---
+++ stack.js
@@ -0,0 +1,42 @@
+class Stack {
+  constructor() {
+    this.items = {};
+    this.head = 0;
+  }
+  push(element) {
+    this.items[this.head] = element;
+    this.head++;
+    return element
+  }
+  pop() {
+    const item = this.items[this.head - 1];
+    delete this.items[this.head - 1];
+    this.head--;
+    return item;
+  }
+  peek() {
+    return this.items[this.head - 1];
+  }
+  size() {
+    return this.head;
+  }
+  isEmpty() {
+    return this.head === 0;
+  }
+  print() {
+    console.log("this.items: ", this.items);
+  }
+}
+
+// const stack1 =new Stack()
+// console.log('stack.push(3): ', stack1.push(5));
+// console.log('stack.push(3): ', stack1.push(34));
+// console.log('stack.push(3): ', stack1.pop());
+// console.log('stack.push(3): ', stack1.push(96));
+// console.log('stack.peek(3): ', stack1.peek());
+// console.log('stack.push(3): ', stack1.push(65));
+// console.log('stack.size(3): ', stack1.size());
+// console.log('stack.push(3): ', stack1.push(93));
+// console.log('stack.isEmpty(3): ', stack1.isEmpty());
+// console.log('stack.pop(3): ', stack1.pop());
+// console.log('stack.print(3): ', stack1.print());
