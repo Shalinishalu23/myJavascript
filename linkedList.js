b043dad3348cdd425be1336800d1fa05850e1716
shalini <shalini.m@ge.com>
2024-09-03 14:52:15 +0530

    Shalini--Updated with tail

--- linkedList.js
+++ linkedList.js
@@ -8,12 +8,14 @@ class Node {
 class LinkedList {
   constructor() {
     this.head = null;
+    this.tail = null;
     this.size = 0;
   }
   prepend(value) {
     const node = new Node(value);
     if (this.isEmpty()) {
       this.head = node;
+      this.tail = node;
     } else {
       node.next = this.head;
       this.head = node;
@@ -34,20 +36,16 @@ class LinkedList {
       str = str + current.value + ",";
       current = current.next;
     }
-    return str
+    return str;
   }
   append(value) {
     const node = new Node(value);
     if (this.isEmpty()) {
       this.head = node;
+      this.tail = node;
     } else {
-      let current = this.head;
-      while (current.next !== null) {
-        current = current.next;
-      }
-      console.log("this.before: ", this.head);
-      current.next = node;
-      console.log("this.after: ", this.head);
+      this.tail.next = node;
+      this.tail = node;
     }
     this.size++;
   }
@@ -121,22 +119,51 @@ class LinkedList {
       current = current.next;
       i++;
     }
-    return -1
+    return -1;
   }
-  reverse(){
-    if(this.isEmpty()){
-      return 'empty so cant reverse'
+  reverse() {
+    if (this.isEmpty()) {
+      return "empty so cant reverse";
     }
-    let prev = null
-    let next
-    let current = this.head
-    while(current){
-      next = current.next
+    let prev = null;
+    let next;
+    let current = this.head;
+    while (current) {
+      next = current.next;
       current.next = prev;
-      prev = current
-      current = next
+      prev = current;
+      current = next;
     }
-    this.head = prev
+    this.tail = this.head;
+    this.head = prev;
+  }
+  removeFromFront() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+    return value;
+  }
+  removeFromEnd() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.tail.value;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let prev = this.head;
+      while (prev.next !== this.tail) {
+        prev = prev.next;
+      }
+      prev.next = null;
+      this.tail = prev;
+    }
+    this.size--;
+    return value;
   }
 }
 
